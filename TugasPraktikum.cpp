f4025fd4f3c22039934840ce70d044018f3dc72b
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-20 14:58:41 +0700

    tugasPraktikum

---
+++ TugasPraktikum.cpp
@@ -0,0 +1,37 @@
+//Library
+#include <iostream>
+using namespace std;
+
+class barang{
+public :
+   string nama;
+   float jumlah;
+   string kategori;
+   string tanggalproduksi;
+
+   void printData(){
+    cout << "Nama : " << nama << endl;
+    cout << "Kategori : " << kategori << endl;
+    cout << "Jumlah : " << jumlah << endl;
+    cout << "TanggalProduksi : " << tanggalproduksi << endl;
+   }
+}; // batas class barang
+
+// mau di tampilkan  elektronik
+int main(){
+    barang elektronik;
+    barang nonElektronik;
+
+    elektronik.nama = "Tablet";
+    elektronik.jumlah = 10;
+    elektronik.kategori = "elektronik";
+    elektronik.tanggalproduksi = "2026-02-29";
+
+    nonElektronik.nama = "Kursi";
+    nonElektronik.jumlah = 20;
+    nonElektronik.kategori = "nonElektronik";
+    nonElektronik.tanggalproduksi = "2026-02-29";
+
+    elektronik.printData();
+    nonElektronik.printData();
+}
